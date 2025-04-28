968e95b9a95868af0d43098948699d242fd0d6d6
Grzegosz <grzegorz.gejlo@cyfroweszkoly.pl>
2025-04-28 16:03:56 +0200

    obsluga players

--- EsportPlayerManager/Models/Player.cs
+++ EsportPlayerManager/Models/Player.cs
@@ -2,5 +2,4 @@ namespace EsportPlayerManager.Models;
 
 public class Player
 {
-    
 }
--- EsportPlayerManager/Views/PlayersView.axaml.cs
+++ EsportPlayerManager/Views/PlayersView.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using EsportPlayerManager.Models;
+using EsportPlayerManager.ViewModels;
 
 namespace EsportPlayerManager.Views;
 
@@ -9,5 +12,25 @@ public partial class PlayersView : UserControl
     public PlayersView()
     {
         InitializeComponent();
+        _viewModel = new PlayerViewModel();
+        this.DataContext = _viewModel;
+    }
+
+    private void AddPlayer_Click(object sender, RoutedEventArgs e)
+    {
+        var newPlayer = new Player
+        {
+            Name = "Nowy",
+            Nickname = "NowyGracz",
+            Game = "Gra",
+            Aim = 50,
+            Strategy = 50,
+            StressLevel = 0,
+            FatigueLevel = 0,
+            //     Experience = 0,
+            //     RankingPoints = 0,
+            //     Money = 1000
+        };
+        _viewModel.AddPlayer(newPlayer);
     }
 }
