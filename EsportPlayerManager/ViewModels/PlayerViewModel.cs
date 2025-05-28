e071bd1f53961601d83993ad1d4633daef368672
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2025-05-29 00:36:10 +0200

    wpisywanie danych ale po kliknieciu przycisku dodaj gracza wywala aplikacje

--- EsportPlayerManager/ViewModels/PlayerViewModel.cs
+++ EsportPlayerManager/ViewModels/PlayerViewModel.cs
@@ -1,46 +1,74 @@
 using System.Collections.ObjectModel;
-using System.Reactive;
-using EsportPlayerManager.Data;
-using EsportPlayerManager.Models;
-using EsportPlayerManager.ViewModels;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Avalonia.Threading;
 using ReactiveUI;
+using EsportPlayerManager.Models;
+using EsportPlayerManager.Data;
+using System.Reactive;
 
-public class PlayerViewModel : ViewModelBase
+namespace EsportPlayerManager.ViewModels
 {
-    private readonly EsportManagerDbContext _context;
-
-    public ObservableCollection<Player> Players { get; }
-    public ReactiveCommand<Unit, Unit> AddPlayerCommand { get; }
-
-    public PlayerViewModel()
+    public class PlayerViewModel : INotifyPropertyChanged
     {
-        _context = new EsportManagerDbContext();
-        Players = new ObservableCollection<Player>(_context.Players);
-        AddPlayerCommand = ReactiveCommand.Create(AddDefaultPlayer);
-    }
+        private readonly EsportManagerDbContext _context;
+        private ObservableCollection<Player> _players;
+        private Player _newPlayer;
 
-    public void AddPlayer(Player player)
-    {
-        _context.Players.Add(player);
-        _context.SaveChanges();
-        Players.Add(player);
-    }
-
-    private void AddDefaultPlayer()
-    {
-        AddPlayer(new Player
+        public ObservableCollection<Player> Players
         {
-            Name = "Nowy",
-            Nickname = "NowyGracz",
-            Game = "CS:GO",
-            Aim = 50,
-            Strategy = 50,
-            Reflex = 50,
-            StressLevel = 0,
-            FatigueLevel = 0,
-            Experience = 0,
-            RankingPoints = 0,
-            Money = 1000
-        });
+            get => _players;
+            set { _players = value; OnPropertyChanged(); }
+        }
+
+        public Player NewPlayer
+        {
+            get => _newPlayer;
+            set { _newPlayer = value; OnPropertyChanged(); }
+        }
+
+        public ReactiveCommand<Unit, Unit> AddPlayerCommand { get; }
+
+        public PlayerViewModel()
+        {
+            _context = new EsportManagerDbContext();
+            Players = new ObservableCollection<Player>(_context.Players);
+            
+            ResetNewPlayer();
+
+            AddPlayerCommand = ReactiveCommand.Create(AddPlayer);
+        }
+
+        private void ResetNewPlayer()
+        {
+            NewPlayer = new Player
+            {
+                Name = string.Empty,
+                Nickname = string.Empty,
+                Game = string.Empty,
+                Aim = 0,
+                Strategy = 0,
+                Reflex = 0,
+                StressLevel = 0,
+                FatigueLevel = 0,
+                Experience = 0,
+                RankingPoints = 0,
+                Money = 0
+            };
+        }
+
+        private void AddPlayer()
+        {
+            _context.Players.Add(NewPlayer);
+            _context.SaveChanges();
+
+            Dispatcher.UIThread.Post(() => Players.Add(NewPlayer));
+
+            ResetNewPlayer();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
--- EsportPlayerManager/Views/PlayersView.axaml.cs
+++ EsportPlayerManager/Views/PlayersView.axaml.cs
@@ -1,39 +1,16 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using EsportPlayerManager.Models;
+using Avalonia.Markup.Xaml;
 using EsportPlayerManager.ViewModels;
 
 namespace EsportPlayerManager.Views
 {
     public partial class PlayersView : UserControl
     {
-        private PlayerViewModel _viewModel;
-
         public PlayersView()
         {
-            InitializeComponent();
+            AvaloniaXamlLoader.Load(this);
+
             DataContext = new PlayerViewModel();
-            
-        }
-
-        private void AddPlayer_Click(object? sender, RoutedEventArgs e)
-        {
-            var newPlayer = new Player
-            {
-                Name = "Nowy",
-                Nickname = "NowyGracz",
-                Game = "CS:GO",
-                Aim = 50,
-                Strategy = 50,
-                Reflex = 50,
-                StressLevel = 0,
-                FatigueLevel = 0,
-                Experience = 0,
-                RankingPoints = 0,
-                Money = 1000
-            };
-
-            _viewModel.AddPlayer(newPlayer);
         }
     }
 }
